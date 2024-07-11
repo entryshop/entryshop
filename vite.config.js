be1a499b26e8615f902800ccc38695924f1cb49c
Alex <alex@parse.cn>
2024-07-11 10:03:56 +0800

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import 'dotenv/config';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    build: {
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
+    }, plugins: [laravel({
+        input: ['resources/js/central/dashboard.js', 'resources/js/tenant/dashboard.js'], refresh: true,
+    }),],
 });
